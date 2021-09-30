87dc78f2c7a3960e37a567bc00b9dd0bbb8b3177
purushottam-gupta <68910707+purushottam-gupta@users.noreply.github.com>
2021-10-01 03:36:29 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+var i = document.getElementById('search')
+
+var r = document.getElementById('rating')
+var votes = document.getElementById('votes')
+var rated = document.getElementById('rated')
+var btn = document.getElementById('btn')
+i.addEventListener('keypress', async (e) => {
+    if (e.code == 'Enter') {
+        try {
+            const v = i.value;
+            var getSelectedValue = document.querySelector('input[name="r"]:checked');
+            if (getSelectedValue != null) {
+                var res = await fetch(`https://www.omdbapi.com/?t=${v}&type=${getSelectedValue.value}&apikey=856a0fc4`)
+            } else {
+                var res = await fetch(`https://www.omdbapi.com/?t=${v}&apikey=856a0fc4`)
+            }
+            var data = await res.json();
+            if(!data.Error && data.imdbRating!=="N/A"){
+            r.innerHTML = data.imdbRating+"/10";
+            votes.innerHTML = data.imdbVotes + " users";
+            rated.innerHTML = data.Rated;
+            } else {
+                r.innerHTML = "Not Found!";
+                votes.innerHTML = "Not Found!";
+                rated.innerHTML = "Not Found!";
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}, true);
+
